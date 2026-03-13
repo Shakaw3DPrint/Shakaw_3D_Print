76e83226303a07adc0db73a2488b8e7a21830ee3
Shakaw3DPrint <shakaw3dprint@hotmail.com>
2026-03-13 12:37:45 -0300

    Update script_os.js

--- script_os.js
+++ script_os.js
@@ -38,7 +38,7 @@ impressora: impressora
 
 }
 
-fetch("https://script.google.com/macros/s/AKfycbz4xp2W2jvV869UAo-c3IlUoipb-2U5xJbuKcdfOTGnBywjUaRy_mgm90sRi2xePGTE/exec",{
+fetch("https://script.google.com/macros/s/AKfycbxJMvFEHKFTyYZqtAh5KnxJZvBszfabxW1IvJiUcYLl456DhhSsttM-c1qu_NQvVAM4/exec",{
 
 method:"POST",
 mode:"no-cors",
