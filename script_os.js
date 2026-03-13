2b5540d48a7d4cf3a0b4dd9bfc2759b51d1e26f2
Shakaw3DPrint <shakaw3dprint@hotmail.com>
2026-03-13 12:16:35 -0300

    Update script_os.js

--- script_os.js
+++ script_os.js
@@ -14,6 +14,24 @@ valor: document.getElementById("valor").value
 
 console.log("Dados enviados:", dados)
 
+  function escolherImpressora(altura,tipo){
+
+if(tipo === "Funcional"){
+return "A1 Mini"
+}
+
+if(altura <= 10){
+return "Mars 3 Pro"
+}
+
+if(altura <= 18){
+return "Saturn 2"
+}
+
+return "Saturn 4 Ultra"
+
+}
+
 fetch("https://script.google.com/macros/s/AKfycbz4xp2W2jvV869UAo-c3IlUoipb-2U5xJbuKcdfOTGnBywjUaRy_mgm90sRi2xePGTE/exec",{
 
 method:"POST",
